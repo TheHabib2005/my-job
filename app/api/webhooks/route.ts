ebcada814f139728255f487fec56c99a160b7abf
HABIB <mdwear2005@gmail.com>
2024-09-15 17:38:48 +0600

    g

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,6 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { createUser } from "@/lib/actions";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -50,10 +51,14 @@ export async function POST(req: Request) {
 
   // Do something with the payload
   // For this guide, you simply log the payload to the console
-  const { id } = evt.data;
+  // const { id } = evt.data;
   const eventType = evt.type;
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
+  // console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  // console.log("Webhook body:", body);
+
+  if (eventType === "user.created") {
+    createUser();
+  }
 
   return new Response("", { status: 200 });
 }
